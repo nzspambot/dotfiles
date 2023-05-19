9b83522140773dfdd7c9ae8082c7ff6b57e03d93
James Baker <jamesb@squareup.com>
2023-05-19 10:41:00 +1000

    added file path to lualine

--- nvim/lua/user/lualine/init.lua
+++ nvim/lua/user/lualine/init.lua
@@ -63,5 +63,14 @@ local bubbles_theme = {
 --  extensions = {},
 --}
 require('lualine').setup({
-    options = { theme = 'nightly' }
+  options = { theme = 'nightly' },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }
+    }
+  }
 })
