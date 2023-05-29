a4ecaee554ffc92f93b565636350b2379fd9aded
James Baker <jamesb@squareup.com>
2023-05-30 09:23:13 +1000

    fiddling with alternative config

--- nvim/lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-require("nightly").setup({
-    transparent = false,
-    highlights = {
-        -- add or override highlights
-        -- Normal = { bg = "#000000" }
-    },
-})
-
-vim.cmd "colorscheme nightly"
