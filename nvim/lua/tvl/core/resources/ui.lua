847322010c141fa116bf5045eaa03e531c480559
James Baker <jamesb@squareup.com>
2023-05-30 14:37:29 +1000

    Noice tweaks

--- nvim/lua/tvl/core/resources/ui.lua
+++ nvim/lua/tvl/core/resources/ui.lua
@@ -368,37 +368,79 @@ return {
 
   -- noicer ui
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      cmdline = {
-        view = "cmdline",
-        format = {
-          cmdline = { icon = "  " },
-          search_down = { icon = "  󰄼" },
-          search_up = { icon = "  " },
-          lua = { icon = " " },
-        },
+     "folke/noice.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify"
       },
-      lsp = {
-        progress = { enabled = true },
-        hover = { enabled = false },
-        signature = { enabled = false },
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            find = "%d+L, %d+B",
+    config = function ()
+      local noice = require("noice")
+      noice.setup({
+        views = {
+          cmdline_popup = {
+            position = {
+              row = 5,
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = "auto",
+            },
+          },
+          popupmenu = {
+            relative = "editor",
+            position = {
+              row = 8,
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = 10,
+            },
+            border = {
+              style = "rounded",
+              padding = { 0, 1 },
+            },
+            win_options = {
+              winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+            },
+          },
+          lsp = {
+            override = {
+              ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+              ["vim.lsp.util.stylize_markdown"] = true,
+              ["cmp.entry.get_documentation"] = true,
+            },
+          },
+          presets = {
+            bottom_search = true, -- use a classic bottom cmdline for search
+            command_palette = false, -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = false, -- add a border to hover docs and signature help
           },
         },
-      },
-    },
+      })
+    end,
   },
+
+  {
+    "mvllow/modes.nvim",
+    config = function ()
+      local modes = require("modes")
+      modes.setup({
+        colors = {
+		      copy = "#f5c359",
+		      delete = "#34eb34",
+		      insert = "#eb4034",
+		      visual = "#9745be",
+	      },
+	      line_opacity = 0.25,
+	      set_cursor = true,
+	      set_cursorline = true,
+	      set_number = true,
+      	ignore_filetypes = { 'NvimTree', 'TelescopePrompt' }
+      })
+    end
+  }
 }
