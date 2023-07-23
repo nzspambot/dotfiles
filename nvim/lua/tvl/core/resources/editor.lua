9412ebdc969c1690ae9761030f6f38ae6dbe4362
James Baker <jamesb@squareup.com>
2023-07-24 09:17:39 +1000

    make neo-tree update on file changes

--- nvim/lua/tvl/core/resources/editor.lua
+++ nvim/lua/tvl/core/resources/editor.lua
@@ -104,6 +104,7 @@ return {
         },
         follow_current_file = true, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
+        use_libuv_file_watcher=true,
         group_empty_dirs = true, -- when true, empty folders will be grouped together
       },
     },
